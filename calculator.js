9fe45525426316d52b4fb40305e0f961cc9f226d
Meredith Britt - Mac <118227496+mfrazb@users.noreply.github.com>
2023-06-12 13:50:12 -0400

    Update UI with calculator buttons

--- calculator.js
+++ calculator.js
@@ -1,22 +1,7 @@
+// create callback functions to perform math operations
+
+
+// create event listeners for clicks and keydowns 
 
 
 
-
-function createMultipleEls(el, num, parent, ...classes) {
-    //input - number of divs to make, parent el to append to, classes array to add
-    
-    let counter = 1;
-
-    while (counter <= num) {
-        const newElement = document.createElement(el);
-        console.log(newElement);
-        classes.forEach(label => newElement.classList.add(label));
-        parent.appendChild(newElement);
-        counter++;
-    }  
-}
-
-// create 11 number buttons 
-const numberPad = document.querySelector(".numbers");
-
-createMultipleEls("div", 11, numberPad, "num-button", "numbers");
